bb24229c0d4a0667aa05f97ec40681f85fa510e8
JuanRuiz <Juanpilef87@gmail.com>
2020-11-26 10:06:59 -0600

    Realizada la ruta de usuario con la funcion POST

---
+++ routes/usuario.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const Usuario = require('../models/usuario');
+const app = express();
+  
+  app.get('/usuario', function (req, res) {
+    res.json({
+      ok: '200',
+      mensaje: 'Usuarios consultados correctamente'
+    });
+  });
+  
+  app.post('/usuario', function (req, res) {
+    let body = req.body;
+    let usr = new Usuario({
+      nombre: body.nombre, 
+      email: body.email,
+      password: body.password
+    });
+
+    usr.save((err, usrDB) =>{
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          msg: 'Ocurrio un error',
+          err
+        });
+      }
+
+      res.json({
+        ok: true,
+        msg: 'Usuario insertado con exito',
+        usrDB
+      });
+    });
+  });
+  
+  app.put('/usuario/:id/:nombre', function (req, res) {
+    let id = req.params.id;
+    let nombre = req.params.nombre;
+  
+    res.json({
+      ok: '200',
+      mensaje: 'Usuario actualizado con exito',
+      id: id,
+      nombre: nombre
+    });
+  });
+  
+  app.delete('/usuario/:id', function(req, res){
+    let id = req.params.id;
+  
+    res.json({
+      ok: '200',
+      mensaje: 'Usuario eliminado con exito',
+      id: id
+    });
+  });
+
+  module.exports = app;
