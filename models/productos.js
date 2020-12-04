8a96b12d0369a83a6763ac699187304ce644dc3f
JuanRuiz <Juanpilef87@gmail.com>
2020-12-03 21:57:19 -0600

    Proyecto (prueba1)

--- models/productos.js
+++ models/productos.js
@@ -1,99 +1,32 @@
-const express = require('express');
-const _ = require('underscore');
-const Productos = require('../models/productos');
-const app = express();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-app.get('/productos', function (req, res) {
-    let desde = req.query.desde || 0;
-    let hasta = req.query.hasta || 5;
-    Productos.find({disponibilidad: true})
-    .skip(Number(desde))
-    .limit(Number(hasta))
-    .exec((err, productos) => {
-      if(err){
-        return res.status(400).json({
-          ok: false,
-          msg: 'Ocurrio un error al momento de consultar los prodcutos',
-          err
-        })
-      }
+let productosSchema = new Schema({
+    nombre: {
+        type: String,
+        unique: true,
+        required: [true, 'Nombre es obligatorio']
+    },
+    categoria: {
+        type: Schema.Types.ObjectId,
+        ref: 'Categoria',
+        required: [true, 'Categoria es obligatoria']
 
-      res.json({
-        ok: true,
-        msg: 'Lista de productos obtenida con exito',
-        conteo: productos.length,
-        productos
-      })
-    });
-  });
-  
-  app.post('/productos', function (req, res) {
-    let body = req.body;
-    let pro = new Productos({
-      nombre: body.nombre, 
-      categoria: body.categoria,
-      precio: body.precio,
-      usuario: body.usuario
-    });
+    },
+    precio: {
+        type: String,
+        required: [true, 'Precio es obligatorio']
+    },
+    disponibilidad: {
+        type: Boolean,
+        default: true
+    },
+    usuario:{
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: [true, 'Usuario es obligatorio']
 
-    pro.save((err, proDB) =>{
-      if(err){
-        return res.status(400).json({
-          ok: false,
-          msg: 'Ocurrio un error al insertar el producto',
-          err
-        });
-      }
-
-      res.json({
-        ok: true,
-        msg: 'Producto insertado con exito',
-        proDB
-      });
-    });
-  });
-  
-  app.put('/productos/:id', function (req, res) {
-   let id = req.params.id;
-   let body = _.pick(req.body, ['nombre', 'categoria', 'precio']);
-
-   Productos.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'}, 
-   (err, proDB) => {
-    if(err){
-      return res.status(400).json({
-        ok: false,
-        msg: 'Ocurrio un error al momento de actualizar el producto',
-        err
-      });
     }
-    res.json({
-      ok: true,
-      msg: 'Producto actualizado con exito',
-      producto: proDB
-    });
-   });
-  });
-  
-  app.delete('/productos/:id', function(req, res){
- // let id = req.params.id;
-   //Usuario.deleteOne({ _id: id }, (err, usrDB) => {
-   
-   //});
-   let id = req.params.id;
-   Productos.findByIdAndUpdate(id, {disponibilidad: false}, {new: true, runValidators: true, context: 'query'}, (err, proDB) => {
-    if(err){
-      return res.status(400).json({
-        ok: false,
-        msg: 'Ocurrio un error al momento de eliminar el producto',
-        err
-      });
-    }
-    res.json({
-      ok: true,
-      msg: 'producto eliminado con exito',
-      proDB
-    });
-   });
-  });
+});
 
-  module.exports = app;
+module.exports = mongoose.model('Productos', productosSchema);
--- routes/productos.js
+++ routes/productos.js
@@ -1,32 +1,96 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const express = require('express');
+const _ = require('underscore');
+const Productos = require('../models/productos');
+const app = express();
 
-let productosSchema = new Schema({
-    nombre: {
-        type: String,
-        unique: true,
-        required: [true, 'El nombre es obligatorio']
-    },
-    categoria: {
-        type: Schema.Types.ObjectId,
-        ref: 'Categoria',
-        required: [true, 'La categoria es obligatoria']
+app.get('/productos', function (req, res) {
+    let desde = req.query.desde || 0;
+    let hasta = req.query.hasta || 5;
+    Productos.find({disponibilidad: true})
+    .skip(Number(desde))
+    .limit(Number(hasta))
+    .exec((err, productos) => {
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          msg: 'Error al consultar prodcutos',
+          err
+        })
+      }
 
-    },
-    precio: {
-        type: String,
-        required: [true, 'El precio es obligatorio']
-    },
-    disponibilidad: {
-        type: Boolean,
-        default: true
-    },
-    usuario:{
-        type: Schema.Types.ObjectId,
-        ref: 'Usuario',
-        required: [true, 'El usuario es obligatorio']
+      res.json({
+        ok: true,
+        msg: 'Lista de productos',
+        conteo: productos.length,
+        productos
+      })
+    });
+  });
+  
+  app.post('/productos', function (req, res) {
+    let body = req.body;
+    let pro = new Productos({
+      nombre: body.nombre, 
+      categoria: body.categoria,
+      precio: body.precio,
+      usuario: body.usuario
+    });
 
+    pro.save((err, proDB) =>{
+      if(err){
+        return res.status(400).json({
+          ok: false,
+          msg: 'Error al insertar producto',
+          err
+        });
+      }
+
+      res.json({
+        ok: true,
+        msg: 'El producto se ha insertado',
+        proDB
+      });
+    });
+  });
+  
+  app.put('/productos/:id', function (req, res) {
+   let id = req.params.id;
+   let body = _.pick(req.body, ['nombre', 'categoria', 'precio']);
+
+   Productos.findByIdAndUpdate(id, body, {new: true, runValidators: true, context: 'query'}, 
+   (err, proDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Error al actualizar producto',
+        err
+      });
     }
-});
+    res.json({
+      ok: true,
+      msg: 'Producto actualizado',
+      producto: proDB
+    });
+   });
+  });
+  
+  app.delete('/productos/:id', function(req, res){
 
-module.exports = mongoose.model('Productos', productosSchema);
+   let id = req.params.id;
+   Productos.findByIdAndUpdate(id, {disponibilidad: false}, {new: true, runValidators: true, context: 'query'}, (err, proDB) => {
+    if(err){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Error al momento de eliminar producto',
+        err
+      });
+    }
+    res.json({
+      ok: true,
+      msg: 'producto eliminado',
+      proDB
+    });
+   });
+  });
+
+  module.exports = app;
