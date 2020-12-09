5274507a039bbfaf116b08a5bc49b49b90154bd1
JuanRuiz <Juanpilef87@gmail.com>
2020-12-08 19:16:24 -0600

    Fina Project

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ require('./config/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors'); 
 const app = express();
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// Habilitar CORS
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+    next();
+});
 
 app.get('/', function(req, res) {
     res.send('<h1>Bienvenido a Starbucks</h1>');
